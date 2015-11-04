c28f61d79d66aa3eea39e94fabafb39c3d750a77
James Tanner <tanner.jc@gmail.com>
2015-11-04 17:42:40 -0500

    Add a new script to set the vagrant public key

--- packer/scripts/cleanup.sh
+++ packer/scripts/cleanup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -eux
 
-# Remove Ansible.
-yum -y remove ansible python-setuptools
-
 # Zero out the rest of the free space using dd, then delete the written file.
+echo "Zero'ing out the rest of the disk ..."
 dd if=/dev/zero of=/EMPTY bs=1M
+echo "Deleting zero'ed tempfile ..."
 rm -f /EMPTY
 
 # Add `sync` so Packer doesn't quit too early, before the large file is deleted.
+echo "Sync'ing to disk"
 sync
