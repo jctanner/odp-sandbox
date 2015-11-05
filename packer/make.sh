bc34906568c81d4082af07fa505b332a2276ef71
James Tanner <tanner.jc@gmail.com>
2015-11-05 10:35:28 -0500

    Switch to the larger 'everything' ISO to improve install times

--- packer/make.sh
+++ packer/make.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+
+rm -rf builds/*
+
 PACKER_LOG=1 PACKER_LOG_PATH=packer.log \
     packer build --only=virtualbox-iso $@ centos7.json 
+RC=$?
+if [[ $RC -ne 0 ]]; then
+    echo "packer build failed"
+    exit $RC
+fi
+
+vagrant box remove odpi-test-0
+vagrant box add --name odpi-test-0 --provider virtualbox builds/virtualbox-centos7.box    
