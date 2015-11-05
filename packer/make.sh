12f41b3622f6b74332e86ba3ba20af0cc5f749d4
James Tanner <tanner.jc@gmail.com>
2015-11-04 19:00:44 -0500

    Use the url method so that the kickstart can find all the packages defined

--- packer/make.sh
+++ packer/make.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-packer build --only=virtualbox-iso centos7.json
+PACKER_LOG=1 PACKER_LOG_PATH=packer.log \
+    packer build --only=virtualbox-iso $@ centos7.json 
--- packer/scripts/vagrant_key.sh
+++ packer/scripts/vagrant_key.sh
@@ -1,8 +1,25 @@
 # Installing vagrant keys
 # source: https://gun.io/blog/building-vagrant-machines-with-packer/
-mkdir /home/vagrant/.ssh
+
+id vagrant
+RC=$?
+if [[ $RC -ne 0 ]]; then
+    useradd vagrant
+fi
+
+echo "vagrant:vagrant" | chpasswd
+
+mkdir -p /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
+
 cd /home/vagrant/.ssh
+
+which wget
+RC=$?
+if [[ $RC -ne 0 ]]; then
+    yum -y install wget
+fi
+
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
 chmod 600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant
