86459825c0cd20a07d395d0d736c941169dca6b0
James Tanner <tanner.jc@gmail.com>
2015-11-10 10:37:14 -0800

    Checkin

--- packer/scripts/ansible.sh
+++ packer/scripts/ansible.sh
@@ -6,5 +6,7 @@ yum -y install epel-release
 # Install Ansible.
 yum -y install ansible python-setuptools
 
-# Run Ansible
-cd /opt/ansible ; ansible-playbook -c local -e "packer=True" -v sandbox.yml
+# Run Ansible [defered to packer's ansible-local provisioner]
+#cd /opt/ansible ; ansible-playbook -c local -e "packer=True" -v sandbox.yml
+touch /var/log/ansible.log
+chmod 777 /var/log/ansible.log
