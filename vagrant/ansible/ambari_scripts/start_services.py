a74865ce03245e3989aeb3118c81dbfa525fa653
James Tanner <tanner.jc@gmail.com>
2015-11-12 08:16:24 -0800

    Fixup the logic for service startup

--- vagrant/ansible/ambari_scripts/start_services.py
+++ vagrant/ansible/ambari_scripts/start_services.py
@@ -21,28 +21,42 @@ def start_services(cluster_name):
     headers = {'X-Requested-By': 'AMBARI'}
     payload = {"ServiceInfo": {"state" : "STARTED"}} 
     for x in services:
-        print x
+        print "# Starting: %s" % x
         r = requests.put(x['href'], auth=('admin', 'admin'), 
                          data=json.dumps(payload), headers=headers)
-        print "# %s" % r.status_code
+        print "# PUT: %s" % r.status_code
         #print "# %s" % r.text
+        #import pdb; pdb.set_trace()
 
-        if int(r.status_code) != 200:
+        # 200 == the service is already started
+        # 201 == the service will be started
+        # 202 == ???
+        if (int(r.status_code) != 200) \
+            and (int(r.status_code) != 201) \
+            and (int(r.status_code) != 202):
+
+            print "# Failure starting %s: %s" % (x, r.status_code)
             print r.text
             sys.exit(1)
 
-        # null text means the service is already running
-        if not r.text:
-            continue
+        # Poll the startup task if the rc was 201
+        if (int(r.status_code) == 201) or (int(r.status_code) == 202):
+            print "# Polling startup for %s" % x
+    
+            #import pdb; pdb.set_trace()
+            # null text means the service is already running
+            #if not r.text:
+            #    print "r.text was null, skipping this service"
+            #    continue
 
-        rdict = json.loads(r.text)
-        #"href" : "http://sandbox.odp.org:8080/api/v1/clusters/ODP_Sandbox/requests/7"
-        #import pdb; pdb.set_trace()
-        print "# Polling %s startup" % x['ServiceInfo']['service_name']
-        import pdb; pdb.set_trace()
-        print rdict
-        if 'href' in rdict:
-            poll_request(rdict['href'])
+            rdict = json.loads(r.text)
+            #"href" : "http://sandbox.odp.org:8080/api/v1/clusters/ODP_Sandbox/requests/7"
+            #import pdb; pdb.set_trace()
+            print "# Polling %s startup" % x['ServiceInfo']['service_name']
+            #import pdb; pdb.set_trace()
+            print rdict
+            if 'href' in rdict:
+                poll_request(rdict['href'])
 
 
 if __name__ == "__main__":
